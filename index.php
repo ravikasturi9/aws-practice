9d18f1fd474ccb63dd03332ca67073529ebeefb6
ravikasturi9 <40877709+ravikasturi9@users.noreply.github.com>
2018-07-11 10:28:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require 'vendor/autoload.php';
 function ServiceHandler() {
     $data = new stdClass();
     $data->Output = "Hello World! this is a DEVOPS Practice Day2, Manager Approved";
-
+    $data->Output = "This is Ravi Kumar Kasturi";
     return json_encode($data);
 };
 
